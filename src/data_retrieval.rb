1c36343c6ad813d462b34da6de09bea837e3f3f2
Oriel <orianafarrugia3@gmail.com>
2021-01-10 00:22:40 -0600

    Refactor to Open connection then perform requests for each searchterm

--- src/data_retrieval.rb
+++ src/data_retrieval.rb
@@ -3,10 +3,24 @@
 require 'rubygems'
 require 'excon'
 
+def expectType(arrayOfTypes, object)
+	for i in arrayOfTypes
+		if object.instance_of? i
+			return i
+		end
+	end
+
+	return false
+end
+
 def search(searchTerm)
+	searchTermType = expectType([String, Array], searchTerm)
+	if searchTermType == Array
+		
+
 	dataSource = "https://www.reversewhois.io/?searchterm=" + searchTerm
 
 	response = Excon.get(dataSource)
 	
-	puts response.body	
+	return response.body	
 end
--- src/test.rb
+++ src/test.rb
@@ -5,7 +5,41 @@ require_relative 'data_retrieval.rb'
 def search_accepts_arbitrary_terms()
 	term1, term2 = "minecraft.net", "nintendo.com"
 
-	puts search(term1) != search(term2)
+	puts (search(term1) != search(term2)).to_s + ": " + __method__.to_s
 end
 
+def expectType_correctly_accepts()
+	aString = "Henlo"
+
+	puts (expectType([String], aString) == String).to_s + ": " + __method__.to_s
+end
+
+def expectType_correctly_declines()
+	aString = "Henlo"
+
+	puts (expectType([Array], aString) == false).to_s + ": " + __method__.to_s
+end
+
+def expectType_accepts_multiple_types()
+	aArray = ["hi", 2]
+
+	puts (expectType([String, Array], aArray) == Array).to_s + ": " + __method__.to_s
+end
+
+def expectType_declines_multiple_types()
+	aArray = ["hi", 2]
+
+	puts (expectType([String, Integer], aArray) == false).to_s + ": " + __method__.to_s
+end
+
+# def search_accepts_multiple_searchTerms()
+# 	term1, term2 = "minecraft.net", "nintendo.com"
+
+# 	puts (search([term1, term2])).to_s + ": " + __method__.to_s
+# end
+
 search_accepts_arbitrary_terms()
+expectType_correctly_accepts()
+expectType_correctly_declines()
+expectType_accepts_multiple_types()
+expectType_declines_multiple_types()
